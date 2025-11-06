3ba069ab9236998bd342c83d39d8f010857a2ec9
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-11-06 16:32:47 -0300

    feat: reemplazar AsideFilterMap con componentes de gráficos en OperatorStatistics para mejorar la visualización de datos

--- src/pages/Operator/OperatorStatistics.jsx
+++ src/pages/Operator/OperatorStatistics.jsx
@@ -1,7 +1,21 @@
-import AsideFilterMap from "../../components/LeafletMaps/AsideFilterMap";
+import ChartDoughnut from "../../components/Chart/ChartDoughnut";
+import ChartLine from "../../components/Chart/ChartLine";
 
 const OperatorStatistics = () => {
-  return <AsideFilterMap />;
+  return (
+    <div className="min-h-screen w-full flex flex-row items-center justify-center gap-[2vw] flex-wrap bg-gray-300">
+      <div className="bg-gray-100 rounded-lg shadow-lg p-4 w-[92%] h-80 flex items-center justify-center">
+        <ChartLine />
+      </div>
+      <div className="bg-gray-100 rounded-lg shadow-lg p-4 w-[92%] h-80 flex items-center justify-center">
+        <ChartLine />
+      </div>
+
+      <div className="bg-gray-100 rounded-lg shadow-lg p-4 w-2/5 h-80 flex items-center justify-center">
+        <ChartDoughnut />
+      </div>
+    </div>
+  );
 };
 
 export default OperatorStatistics;
